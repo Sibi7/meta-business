17013ff2ddda9d34bc454053f0158d1f93955d17
Vladimir__Lipodat <sibi_7@mail.ru>
2017-08-14 10:25:55 +0300

    add link scroll

--- app/js/script.js
+++ app/js/script.js
@@ -8,5 +8,21 @@ $(document).ready(function () {
         $('html,body').animate({scrollTop: top}, 1000);
         return false;
     });
+    $(document).on('click', '.footer__menu a', function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        var target = $(href);
+        var top = target.offset().top;
+        $('html,body').animate({scrollTop: top}, 1000);
+        return false;
+    });
+    $(document).on('click', '.header__wrapper__navigation a', function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        var target = $(href);
+        var top = target.offset().top;
+        $('html,body').animate({scrollTop: top}, 1000);
+        return false;
+    });
     //scroll link end
 });
