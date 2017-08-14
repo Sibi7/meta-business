81bc623150a3fd6c7b3dd452d5828221058cc4d3
Vladimir__Lipodat <sibi_7@mail.ru>
2017-08-14 16:51:22 +0300

    add animation, fix bug

--- app/js/script.js
+++ app/js/script.js
@@ -25,6 +25,151 @@ $(document).ready(function () {
         return false;
     });
     //scroll link end
+    //animate element 'animate css'
+    //animate text header
+    $('.header__text h1').each(function () {
+        $(this).addClass('animated bounceInDown').css('opacity', '1');
+    });
+    $('.header__text h2').each(function () {
+        $(this).addClass('animated bounceInUp').css('opacity', '1');
+    });
+    /*clouds animation*/
+    $(window).scroll(function () {
+
+        $('.quote__wrapper').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1');
+            }
+        });
+        $('.what-learn').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1');
+            }
+        });
+        $('.results-people h1').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass('animated fadeInLeft').css('opacity', '1');
+            }
+        });
+        $('.results-people h2').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass('animated fadeInRight').css('opacity', '1');
+            }
+        });
+        $('.useful h1').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass('animated fadeInLeft').css('opacity', '1');
+            }
+        });
+        $('.useful__wrapper__item').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1');
+            }
+        });
+        $('.results-people__stats-people ').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1');
+            }
+        });
+        $('.results-people__wrapper ').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1');
+            }
+        });
+        $('.results-people__stats ').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1');
+            }
+        });
+        $('.results-people ul').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1');
+            }
+        });
+        //end img----------------------------
+
+        //dot----------------------------
+        $('.better__block--dot').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 500) {
+                $(this).css('opacity', '1')
+                    .addClass('fadeInUpBig')
+                    .css('height', '250px');
+            }
+        });
+        //end dot----------------------------
+
+        //text----------------------------
+        $('.better__block--text').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1')
+                    .addClass('animated zoomIn')
+            }
+        });
+        //end text----------------------------
+
+        //time-and-money----------------------------
+        $('.time-money__left p img').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1').addClass('animated rotateIn')
+            }
+        });
+        $('.classes h1').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass('animated fadeInLeft').css('opacity', '1');
+            }
+        });
+        $('.classes__item').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1');
+            }
+        });
+        $('.classes-second-day__item').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1');
+            }
+        });
+        $('.quote-bottom').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).css('opacity', '1');
+            }
+        });
+        //end time-and-money----------------------------
+
+    });
 });
 //tabs
 (function ($) {
